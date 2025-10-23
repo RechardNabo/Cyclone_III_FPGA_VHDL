8092e8941981cbcde8bda21ba6617dd651513a4c
Rechard Nabo <32723733+RechardNabo@users.noreply.github.com>
2025-10-23 09:08:07 +0200

    added more empty projects

---
+++ src/MCU_Architectures/Renesas_Synergy/Synergy-S6/synergy_s6_interface.vhd
@@ -0,0 +1,14 @@
+-- Renesas Synergy S6 Interface VHDL File
+-- This file contains the interface definition for Renesas Synergy S6 series MCU
+-- 
+-- Author: [To be filled]
+-- Date: [To be filled]
+-- Description: Interface module for Synergy S6 MCU integration
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Entity declaration will be added here
+-- Interface signals and ports will be defined here
+-- Implementation to be completed
---
+++ src/MCU_Architectures/Renesas_Synergy/Synergy-S6/synergy_s6_testbench.vhd
@@ -0,0 +1,14 @@
+-- Renesas Synergy S6 Testbench VHDL File
+-- This file contains the testbench for Renesas Synergy S6 series MCU interface
+-- 
+-- Author: [To be filled]
+-- Date: [To be filled]
+-- Description: Testbench for Synergy S6 MCU interface verification
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Testbench entity declaration will be added here
+-- Test procedures and stimulus generation will be defined here
+-- Implementation to be completed
